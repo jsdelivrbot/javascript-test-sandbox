b5792dd0a1fd851acc9143666bf1f818b2045931
yinm <yinm.cminor9@gmail.com>
2018-04-26 21:02:20 +0900

    Check assert library for writing tests simply

--- 01_introduce-frontend-tests/test/calc-util-spec.js
+++ 01_introduce-frontend-tests/test/calc-util-spec.js
@@ -1,18 +1,13 @@
 // 2nd
+const assert = require('assert')
 const calcUtil = require('../src/calc-util.js')
 
 describe('add関数のテスト', () => {
   it('1 + 2 は 3 である', () => {
-    if (calcUtil.add(1, 2) === 3) {
-    } else {
-      throw new Error('失敗')
-    }
+    assert(calcUtil.add(1, 2) === 3)
   })
 
   it('1 + 2 は 4 である', () => {
-    if (calcUtil.add(1, 2) === 4) {
-    } else {
-      throw new Error('失敗')
-    }
+    assert(calcUtil.add(1, 2) === 4)
   })
 })
