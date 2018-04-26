ee8cd1aa04d2ed69bc8ee57088ef441c9f78b196
yinm <yinm.cminor9@gmail.com>
2018-04-26 20:41:16 +0900

    Check test with Node.js

--- 01_introduce-frontend-tests/test/calc-util-spec.js
+++ 01_introduce-frontend-tests/test/calc-util-spec.js
@@ -1,13 +1,15 @@
+const calcUtil = require('../src/calc-util.js')
+
 describe('add関数のテスト', () => {
   it('1 + 2 は 3 である', () => {
-    if (add(1, 2) === 3) {
+    if (calcUtil.add(1, 2) === 3) {
     } else {
       throw new Error('失敗')
     }
   })
 
   it('1 + 2 は 4 である', () => {
-    if (add(1, 2) === 4) {
+    if (calcUtil.add(1, 2) === 4) {
     } else {
       throw new Error('失敗')
     }
