ad7d704292bcba245ad55e974f736423400a910f
yinm <yinm.cminor9@gmail.com>
2018-04-26 22:07:06 +0900

    Check using framework and registering files

--- 02_step-by-step-karma/karma.conf.js
+++ 02_step-by-step-karma/karma.conf.js
@@ -1,5 +1,10 @@
 module.exports = (config) => {
   config.set({
+    frameworks: ['jasmine'],
+    files: [
+      'app/app.js',
+      'test/*Spec.js',
+    ],
     logLevel: config.LOG_DEBUG
   })
 }
