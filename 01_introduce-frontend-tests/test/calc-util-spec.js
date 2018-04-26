46f265052a127438a95da85a8288d61505629093
yinm <yinm.cminor9@gmail.com>
2018-04-26 20:36:07 +0900

    Check tests with testing framework (mocha)

--- 01_introduce-frontend-tests/test/calc-util-spec.js
+++ 01_introduce-frontend-tests/test/calc-util-spec.js
@@ -1,8 +1,15 @@
-console.log('add関数のテスト')
-console.log('1 + 2 は 3 である')
+describe('add関数のテスト', () => {
+  it('1 + 2 は 3 である', () => {
+    if (add(1, 2) === 3) {
+    } else {
+      throw new Error('失敗')
+    }
+  })
 
-if (add(1, 2) === 3) {
-  console.info('成功')
-} else {
-  console.error('失敗')
-}
+  it('1 + 2 は 4 である', () => {
+    if (add(1, 2) === 4) {
+    } else {
+      throw new Error('失敗')
+    }
+  })
+})
