b0ce9059a488a284ecf94e981cd60a9f95f6ba7e
yinm <yinm.cminor9@gmail.com>
2018-06-12 21:32:35 +0900

    Check stub

--- 03_typescript-webpack4-karma-sinon/src/index.spec.ts
+++ 03_typescript-webpack4-karma-sinon/src/index.spec.ts
@@ -15,9 +15,33 @@ it('spy', () => {
 
   const obj: any = {
     method() {
-      console.log('called.')
+      console.log('called spy.')
     }
   }
   sinon.spy(obj, 'method')
+  obj.method()
+})
+
+it('stub', () => {
+  const item = new Item()
+  const stub = sinon.stub(item, 'calculateDiscount')
+
+  stub.onCall(0).returns(70)
+  stub.onCall(1).returns(80)
+
+  const order = new Order()
+  order.add(item)
+  order.add(item)
+  order.pay(new Date(), 20)
+
+  expect(stub.withArgs(20).calledTwice).toBe(true)
+  expect(order.getPayment()).toBe(150)
+
+  const obj: any = {
+    method() {
+      console.log('called stub.')
+    }
+  }
+  sinon.stub(obj, 'method')
   obj.method()
 })
